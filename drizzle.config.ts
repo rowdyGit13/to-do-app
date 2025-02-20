9e5d7246f762e28ed18ad8b4d9b64ca5fc23d13c
rowdyGit13 <127695758+rowdyGit13@users.noreply.github.com>
2025-02-20 17:26:39 -0600

    reinstall drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,15 @@
 import { config } from "dotenv";
-import { defineConfig } from "drizzle-kit";
+import { DrizzleConfig } from "drizzle-kit";
 
 config({ path: ".env.local" });
 
-export default defineConfig({
+const config: DrizzleConfig = {
   schema: "./db/schema/index.ts",
   out: "./db/migrations",
   dialect: "postgresql", //tells drizzle what language we are using
   dbCredentials: {
     url: process.env.DATABASE_URL! //allows us to inject environmental variable securely while comitting this file to git
   }
-});
+};
+
+export default config;
